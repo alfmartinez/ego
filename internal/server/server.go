3a6a58d48a2a1b7f64c5adedef4579eddba0f9e1
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-04 17:49:06 +0100

    Sample routes

--- internal/server/server.go
+++ internal/server/server.go
@@ -17,12 +17,23 @@ func CreateServer() Server {
 type server struct{}
 
 func (s *server) Start(addr string) {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
-	})
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/render", renderHandler)
 
 	fmt.Printf("Starting server at address %s \n", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Index")
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status")
+}
+func renderHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Render")
 }
