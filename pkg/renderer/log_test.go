3f0278cda99485ad52ef6969d4d6fa143c1d25c5
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-04 16:46:25 +0100

    Test Log Renderer

---
+++ pkg/renderer/log_test.go
@@ -0,0 +1,33 @@
+package renderer
+
+import (
+	"ego/pkg/configuration"
+	"image"
+	"testing"
+)
+
+type fakeRenderable struct{}
+
+func (f *fakeRenderable) Name() string          { return "fake" }
+func (f *fakeRenderable) Doing() string         { return "fake" }
+func (f *fakeRenderable) Position() image.Point { return image.Pt(0, 0) }
+func (f *fakeRenderable) Path() string          { return "fake" }
+
+func (f *fakeRenderable) Size() uint { return 1 }
+
+func TestLogRenderer(t *testing.T) {
+	r := CreateRenderer(configuration.Renderer{
+		Type: "log",
+	})
+
+	if r.IsAsync() {
+		t.Error("Log renderer should not be async")
+	}
+	r.Init()                 // Do nothing
+	r.Start(make(chan bool)) // Do nothing
+	r.Refresh()              // Do nothing
+
+	s := &fakeRenderable{}
+
+	r.Render(s)
+}
