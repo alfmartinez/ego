0cf4a386a61d1359a03b6482c68f1b57f95f6fb9
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-04 15:05:00 +0100

    Test IdleState

---
+++ pkg/state/idle_test.go
@@ -0,0 +1,44 @@
+package state
+
+import (
+	"ego/pkg/motivator"
+	"ego/pkg/terrain"
+	"reflect"
+	"testing"
+)
+
+type fakeIdler struct {
+	motivator.NeedsCollection
+}
+
+func createFakeIdler(need string) Idler {
+	nc := motivator.CreateNeedsCollection()
+	nc.AddNeed(motivator.CreateNeed(need, 0), 10)
+	return &fakeIdler{nc}
+}
+
+func TestIdleState(t *testing.T) {
+	s := CreateState("idle")
+	g := terrain.CreateGrid(1, 1)
+
+	cases := []struct {
+		need              string
+		expectedInterface string
+	}{
+		{"curiosity", "*state.exploreState"},
+		{"social", "*state.interactState"},
+		{"rest", "*state.restState"},
+		{"health", "*state.healState"},
+	}
+
+	for _, x := range cases {
+		sm := createFakeIdler(x.need)
+		next := s.Update(sm, g)
+
+		if reflect.TypeOf(next).String() != x.expectedInterface {
+			t.Errorf("Expected state %+v, got %+v", x.expectedInterface, reflect.TypeOf(next).String())
+		}
+
+	}
+
+}
