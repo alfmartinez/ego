2dfda93cb08e5351011c3fcf8b57da4d69003231
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-08 12:13:04 +0100

    Complete tests in game package

--- pkg/game/game_test.go
+++ pkg/game/game_test.go
@@ -7,14 +7,27 @@ type fakeGame struct{}
 func (f *fakeGame) Start() {}
 func (f *fakeGame) Stop()  {}
 
-func TestCreateGameUsesRegisterFactory(t *testing.T) {
+func TestCreateGame(t *testing.T) {
 	RegisterGameFactory("test", func() Game {
 		return &fakeGame{}
 	})
-	game := CreateGame("test")
 
-	_, ok := game.(*fakeGame)
-	if !ok {
-		t.Errorf("CreateGame should have returned FakeGame")
-	}
+	t.Run("uses Registered Factories", func(t *testing.T) {
+		game := CreateGame("test")
+
+		_, ok := game.(*fakeGame)
+		if !ok {
+			t.Errorf("CreateGame should have returned FakeGame")
+		}
+	})
+
+	t.Run("uses file Factory", func(t *testing.T) {
+		game := CreateGame("file")
+
+		_, ok := game.(*sampleGame)
+		if !ok {
+			t.Errorf("CreateGame should have returned sampleGame")
+		}
+	})
+
 }
