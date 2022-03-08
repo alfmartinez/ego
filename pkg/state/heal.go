94f10aaf9e0b2cc19a678349df0416e1f46f3a90
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-08 17:19:47 +0100

    Test Mark

--- pkg/state/heal.go
+++ pkg/state/heal.go
@@ -14,7 +14,7 @@ func init() {
 
 type Patient interface {
 	movement.Positionnable
-	motivator.NeedsCollection
+	Provide(motivator.Need, int, int)
 }
 
 type healState struct {
@@ -22,22 +22,5 @@ type healState struct {
 }
 
 func (s *healState) Update(a Updatable) State {
-	g := terrain.GetTerrain()
-	patient := a.(Patient)
-	if s.target == nil {
-		tiles := g.FindClosest(patient, 1, func(t terrain.Tile) bool {
-			return t.HasResource(terrain.Health)
-		})
-		tile := tiles[0]
-		if tile != nil {
-			s.target = tile
-		}
-	}
-	if patient.IsAt(s.target) {
-		patient.Provide(motivator.Health, 1, 10)
-		s.target.Consume(terrain.Health)
-	} else {
-		return CreateState("move", MoveArguments{Destination: s.target, Next: s})
-	}
-	return CreateState("idle")
+	return nil
 }
---
+++ pkg/state/heal_test.go
@@ -0,0 +1,10 @@
+package state
+
+import "testing"
+
+func TestHealState(t *testing.T) {
+
+	t.Run("if character has medicine, consume it", func(t *testing.T) {
+		t.Error("Implement gear and items and come back")
+	})
+}
