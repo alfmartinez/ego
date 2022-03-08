02ae3697b421085003616f5814118a05a4ea1524
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-08 12:36:56 +0100

    Test StateMob

--- pkg/object/object_test.go
+++ pkg/object/object_test.go
@@ -5,18 +5,40 @@ import (
 	"testing"
 )
 
-func TestNew(t *testing.T) {
-	var config = configuration.Mob{
-		Type: "Mob",
-		Needs: []configuration.Need{{
-			Type:     "building mocks",
-			Priority: 0,
-			Level:    100,
-		}},
-	}
+type fakeObject struct{}
 
-	actual := CreateObject(config)
-	if _, ok := actual.(StateMob); !ok {
-		//t.Errorf("Should return StateMob, got %+v", actual)
-	}
+func (f fakeObject) Update() {}
+
+func TestCreateObject(t *testing.T) {
+
+	RegisterObjectFactory("fake", func(m configuration.Mob) GameObject {
+		return fakeObject{}
+	})
+
+	t.Run("Creates object base on type : 'fake' ", func(t *testing.T) {
+		var config = configuration.Mob{
+			Type: "fake",
+		}
+
+		actual := CreateObject(config)
+		if _, ok := actual.(fakeObject); !ok {
+			t.Errorf("Should return fakeObject, got %+v", actual)
+		}
+	})
+
+	t.Run("Creates object base on type : 'Mob' ", func(t *testing.T) {
+		var config = configuration.Mob{
+			Type: "Mob",
+			Needs: []configuration.Need{{
+				Type:     "building mocks",
+				Priority: 0,
+				Level:    100,
+			}},
+		}
+
+		actual := CreateObject(config)
+		if _, ok := actual.(StateMob); !ok {
+			t.Errorf("Should return StateMob, got %+v", actual)
+		}
+	})
 }
---
+++ pkg/object/state_mob_test.go
@@ -0,0 +1,28 @@
+package object
+
+import (
+	"ego/pkg/configuration"
+	"testing"
+)
+
+func TestStateMob(t *testing.T) {
+	var config = configuration.Mob{
+		Type: "Mob",
+		Needs: []configuration.Need{{
+			Type:     "building mocks",
+			Priority: 0,
+			Level:    100,
+		}},
+	}
+	t.Run("CreateStateMob creates Mob from configuration", func(t *testing.T) {
+		o := CreateStateMob(config)
+		if _, ok := o.(*stateMob); !ok {
+			t.Errorf("Mob should be StateMob, got %+v", o)
+		}
+	})
+	t.Run("StateMob delegates Update to StateMachine", func(t *testing.T) {
+		o := CreateStateMob(config)
+		o.Update()
+		// Checking delegation to do
+	})
+}
