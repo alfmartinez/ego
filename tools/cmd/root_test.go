1bb65b7e3ee6ae33fde7717e21c2a80255d4b02e
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-15 13:27:18 +0100

    Test Cmd with Example

---
+++ tools/cmd/root_test.go
@@ -0,0 +1,22 @@
+package cmd
+
+func ExampleExecute() {
+	Execute()
+	// Output:
+	// A toolset designed to provide game building abilities
+	//
+	// Usage:
+	//   tools [command]
+	//
+	// Available Commands:
+	//   completion  Generate the autocompletion script for the specified shell
+	//   help        Help about any command
+	//   ssheet      A sprite sheet building tool
+	//
+	// Flags:
+	//   -h, --help     help for tools
+	//   -t, --toggle   Help message for toggle
+	//
+	// Use "tools [command] --help" for more information about a command.
+
+}
