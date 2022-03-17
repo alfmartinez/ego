a92a86be1727d662d9d6e998812f1bb97bac262e
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-17 16:36:27 +0100

    Test Observer

--- pkg/observer/observer_test.go
+++ pkg/observer/observer_test.go
@@ -12,17 +12,49 @@ func (o *mockObserver) OnNotify(e Event) {
 }
 
 func TestObserver(t *testing.T) {
-	var called bool
-	evt := CreateEvent(UPDATE, nil)
-	s := CreateSubject()
-	s.Register(&mockObserver{func(e Event) {
-		called = true
-		if e.Type() != evt.Type() {
-			t.Errorf("Observer should be notified about %v, received %v", evt, e)
+	t.Run("Simple observer", func(t *testing.T) {
+		var called bool
+		evt := CreateEvent(UPDATE, nil)
+		s := CreateSubject()
+		s.Register(&mockObserver{func(e Event) {
+			called = true
+			if e.Type() != evt.Type() {
+				t.Errorf("Observer should be notified about %v, received %v", evt, e)
+			}
+		}})
+		s.NotifyAll(evt)
+		if !called {
+			t.Error("Observer should have been notified")
 		}
-	}})
-	s.NotifyAll(evt)
-	if !called {
-		t.Error("Observer should have been notified")
-	}
+	})
+
+	t.Run("Two observers", func(t *testing.T) {
+		var called1, called2 int
+		evt := CreateEvent(UPDATE, nil)
+		s := CreateSubject()
+		o1 := &mockObserver{func(e Event) {
+			called1++
+			if e.Type() != evt.Type() {
+				t.Errorf("Observer should be notified about %v, received %v", evt, e)
+			}
+		}}
+		o2 := &mockObserver{func(e Event) {
+			called2++
+			if e.Type() != evt.Type() {
+				t.Errorf("Observer should be notified about %v, received %v", evt, e)
+			}
+		}}
+		s.Register(o1)
+		s.Register(o2)
+		s.NotifyAll(evt)
+		s.Unregister(o2)
+		s.NotifyAll(evt)
+		if called1 != 2 {
+			t.Errorf("Observer 1 should have been notified twice, got %d", called1)
+		}
+		if called2 != 1 {
+			t.Errorf("Observer 2 should have been notified once, got %d", called2)
+		}
+	})
+
 }
