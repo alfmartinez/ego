efc048a22bc0dee1edf23367956b06ecfacf9675
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-20 00:35:23 +0100

    Test Display

--- engine/display/display_test.go
+++ engine/display/display_test.go
@@ -1,10 +1,10 @@
 package display
 
 import (
+	"ego/engine/configuration"
+	"ego/engine/context"
 	"image"
 	"testing"
-
-	"github.com/spf13/viper"
 )
 
 type FakeDisplay struct {
@@ -14,16 +14,32 @@ func (f *FakeDisplay) Init() {}
 func (f *FakeDisplay) Render() image.Image {
 	return &image.RGBA{}
 }
-func (f *FakeDisplay) AddObject(s Displayable) {}
+func (f *FakeDisplay) AddObject(s any) {}
 
 func TestRegisterDisplayAllowsCreation(t *testing.T) {
+	context.CreateAndRegisterContext("test")
+	cfg := configuration.CreateConfiguration("").Get()
+	context.Set("cfg", cfg)
 	RegisterDisplay("fake", func() Display {
 		return &FakeDisplay{}
 	})
-	viper.Set("renderer.display.type", "fake")
-	display := CreateDisplay()
-	s, ok := display.(*FakeDisplay)
-	if !ok {
-		t.Errorf("Should return FakeDisplay, got %+v", s)
-	}
+	t.Run("ok", func(t *testing.T) {
+		cfg.Set("renderer.display.type", "fake")
+		display := CreateDisplay()
+		s, ok := display.(*FakeDisplay)
+		if !ok {
+			t.Errorf("Should return FakeDisplay, got %+v", s)
+		}
+	})
+	t.Run("ko", func(t *testing.T) {
+		cfg.Set("renderer.display.type", "foo")
+		defer func() {
+			if r := recover(); r == nil {
+				t.Error("should panic")
+			}
+		}()
+		CreateDisplay()
+
+	})
+
 }
