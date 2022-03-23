ff85ce7735f1aef37540b48b75063e65533b8917
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-23 12:29:09 +0100

    Inventory in progress

--- engine/text/grammar.go
+++ engine/text/grammar.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Grammar struct {
-	Pos    lexer.Position
-	Title  string   `@String`
-	Define []Define `@@*`
+	Pos       lexer.Position
+	Title     string    `@String`
+	Define    []Define  `@@*`
+	Inventory Inventory `@@`
 }
 
 type Define struct {
@@ -17,21 +18,36 @@ type Define struct {
 }
 
 type Value struct {
+	Pos  lexer.Position
 	Room bool `(@Room`
 	Prop bool `| @Prop)`
+}
+
+type Inventory struct {
+	Pos       lexer.Position
+	Inventory []string `"Le personnage possède" @Item*`
 }
 
 var (
 	def = lexer.MustStateful(lexer.Rules{
 		"Root": {
-			{"punct", `\.`, nil},
+			{"punct", `[\.\,]+`, nil},
 			{"whitespace", `\s+`, nil},
 			{"nl", `\n`, nil},
 			{"String", `"(\\"|[^"])*"`, nil},
+			{"Inventory", "Le personnage possède", lexer.Push("Inventory")},
 			{"Ident", `[A-Z][^\s]+`, nil},
 			{"Room", `est une pièce`, nil},
 			{"Prop", `est là`, nil},
 			{"CatchAll", `.*`, nil},
+		},
+		"Inventory": {
+			{"separatorVirgule", `\,`, nil},
+			{"separatorEt", `et`, nil},
+			{"Item", `[^\,]+`, nil},
+			{"InventoryEnd", `\.`, lexer.Pop()},
+			lexer.Include("Root"),
+			lexer.Return(),
 		},
 	})
 )
