e307fb5d70d4bbe8b41d5065100bc7547bbf4d65
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-23 16:39:50 +0100

    Parse Title OK

--- engine/text/grammar.go
+++ engine/text/grammar.go
@@ -5,47 +5,18 @@ import (
 )
 
 type Grammar struct {
-	Pos       lexer.Position
-	Title     string    `@String`
-	Define    []Define  `@@*`
-	Inventory Inventory `@@`
-}
-
-type Define struct {
-	Pos   lexer.Position
-	Ident []string `@Ident+`
-	Value Value    `@@`
-}
-
-type Value struct {
-	Pos  lexer.Position
-	Room bool `(@Room`
-	Prop bool `| @Prop)`
-}
-
-type Inventory struct {
-	Pos   lexer.Position
-	Items []string `"Le personnage possède " @Item* "."`
+	Title string `@String`
 }
 
 var (
 	def = lexer.MustStateful(lexer.Rules{
 		"Root": {
+			{"comment", `\/\/.+\n`, nil},
 			{"punct", `[\.\,\s]+`, nil},
 			{"whitespace", `\s+`, nil},
 			{"nl", `\n`, nil},
 			{"String", `"(\\"|[^"])*"`, nil},
-			{"Inventory", "Le personnage possède ", lexer.Push("Inventory")},
-			{"Ident", `[A-Z][^\s]+`, nil},
-			{"Room", `est une pièce`, nil},
-			{"Prop", `est là`, nil},
-			{"CatchAll", `.*`, nil},
-		},
-		"Inventory": {
-			{"separatorComma", `(\,|\s)`, nil},
-			{"And", `\bet\b`, nil},
-			{"Item", `[\p{L}\s]+`, nil},
-			{"InventoryEnd", `\.`, lexer.Pop()},
+			{"Ident", `([\p{L}]+\s*)+`, nil},
 		},
 	})
 )
--- engine/text/parser_test.go
+++ engine/text/parser_test.go
@@ -21,17 +21,14 @@ func TestParse(t *testing.T) {
 			},
 			&Grammar{
 				Title: "Bic Example",
-				Inventory: Inventory{
-					Items: []string{"une orange", "un stylo bic", "une serviette en papier"},
-				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Parse(tt.args.filepath)
-			if !reflect.DeepEqual(got.Inventory.Items, tt.want.Inventory.Items) {
-				t.Errorf("Parse() = %q, want %q", got.Inventory.Items, tt.want.Inventory.Items)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Define = %+v, want %+v", got, tt.want)
 			}
 		})
 	}
