33bd5293a8f4b31e8e84873a88635d24e05fa5ed
alfmartinez <alexandre.martinez76@gmail.com>
2022-03-28 16:08:20 +0200

    Add informer rules in comment

--- engine/text/grammar.go
+++ engine/text/grammar.go
@@ -4,6 +4,16 @@ import (
 	"github.com/alecthomas/participle/v2/lexer"
 )
 
+// Defines a grammar for natural text parsing.
+// 1. 	S = DP + VP 			: The Chamber is a room.
+// 2. 	VP = Verb + DP			: is a room
+// 3a. 	DP = DP + which + VP	: The cart is a vehicle which is movable.
+// 3b. 	DP = DP + who + VP		: The Janitor is a male person who has the blue key.
+// 4. 	DP = DP + RP 			: An open container in the ballroom
+// 5a. 	RP = Preposition + DP 	: in a container
+// 5b. 	RP = Participle + DP 	: worn by Mr Darcy
+// 6 	S = RP + Verb + DP 		: In the Garden is a sunflower
+
 type (
 	// Define a Grammar for parsing
 	Grammar struct {
